c794d236d1607d316b8016afef6d126550f6ffb6
Chidi Nwanneka <cnwanneka@gmail.com>
2023-02-19 07:43:12 +0000

    created a textarea and button

--- script.js
+++ script.js
@@ -7,4 +7,19 @@ let timeInterval;
 
 
 
+
+
+
+// Getting the button that was clicked.
+
+$("button").on("click", function(event){
+
+    console.log($(event.target).siblings("textarea").val())
+
+
+})
+
+
+
+
 });
